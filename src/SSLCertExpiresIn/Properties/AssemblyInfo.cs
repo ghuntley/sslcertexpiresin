88ae364e28e706cf42ef5828b9078ea40e00fda4
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-03-06 09:00:05 +1100

    library is not exposed to comm

--- src/SSLCertExpiresIn/Properties/AssemblyInfo.cs
+++ src/SSLCertExpiresIn/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,29 +6,20 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SSLCertExpiresIn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Geoffrey Huntley")]
 [assembly: AssemblyProduct("SSLCertExpiresIn")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("898037b9-b48f-4c2c-875c-a92649d7f8ac")]
-
-// Version information for an assembly consists of the following four values:
+// Version information for an assembly uses SemVer:
 //
 //      Major Version
 //      Minor Version 
-//      Build Number
-//      Revision
+//      Patch Version
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
